8f32d50c4754a772a9b9b1d01af89ce617a12eab
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2023-05-28 19:29:04 +0300

    updated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,20 +18,20 @@ class FlaskAppTests(unittest.TestCase):
         self.assertEqual(response.data.decode('utf-8'), expected_result)
 
     def test_add_numbers(self):
-        data = {'num1': 7, 'num2': 8}
+        data = {'num1': 3, 'num2': 7}
         response = self.app.post('/api/add', json=data)
         result = response.get_json().get('result', '')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(result, 15)
+        self.assertEqual(result, 10)
 
     def test_multiply_numbers(self):
-        data = {'num1': 7, 'num2': 8}
+        data = {'num1': 6, 'num2': 9}
         response = self.app.post('/api/multiply', json=data)
         result = response.get_json().get('result', '')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(result, 56)
+        self.assertEqual(result, 54)
 
 
 if __name__ == '__main__':
